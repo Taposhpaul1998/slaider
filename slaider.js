08fe6e40faace44462520d96792518264b0f43c2
Taposhpaul <taposhp05@gmail.com>
2022-03-12 13:02:55 +0600

    first commit

---
+++ slaider.js
@@ -0,0 +1,16 @@
+const images = [
+    ' image/money (1).jpg',
+    ' image/money (2).jpg',
+    ' image/money (3).jpg',
+    ' image/money (4).jpg',
+]
+let imgIndex = 0;
+const imgElement = document.getElementById('slaider');
+setInterval(() => {
+    if (imgIndex >= images.length) {
+        imgIndex = 0
+    }
+    const imgUrl = images[imgIndex]
+    imgElement.setAttribute('src', imgUrl)
+    imgIndex++
+}, 2000)
